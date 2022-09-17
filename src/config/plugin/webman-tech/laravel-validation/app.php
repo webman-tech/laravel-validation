3d0e568ffbb1675af2e185f5c37721dceb5fdd54
kriss <462679766@qq.com>
2022-09-17 12:01:46 +0800

    feature: add NullTranslator

--- src/config/plugin/webman-tech/laravel-validation/app.php
+++ src/config/plugin/webman-tech/laravel-validation/app.php
@@ -1,11 +1,13 @@
 <?php
 
-use Illuminate\Contracts\Translation\Translator as TranslatorContract;
 use support\Container;
 
 return [
     'enable' => true,
     'translation' => function () {
-        return Container::get(TranslatorContract::class);
+        // 不处理验证消息提示，需要自定义消息类型
+        return Container::get(\WebmanTech\LaravelValidation\Translation\NullTranslator::class);
+        // 实现过 Translator 的，可以使用类似下面的方式来返回 Translator 实例
+        //return Container::get(Illuminate\Contracts\Translation\Translator::class);
     }
 ];
